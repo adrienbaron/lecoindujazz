cd10fe7043266d370f504202de9a6c59bf078dde
adrienbaron <adrien.baron@tractable.ai>
2023-04-07 10:27:56 +0100

    use remix new dev mode

--- remix.config.js
+++ remix.config.js
@@ -4,8 +4,7 @@ module.exports = {
   server: "./server.js",
   devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "functions/[[path]].js",
-  // publicPath: "/build/",
+  future: {
+    unstable_dev: true,
+  },
 };
