ac6e7d28063eff0dfdba6e9bc44ada301124ba51
adrienbaron <adrien.baron@tractable.ai>
2023-04-16 15:16:23 +0100

    improve error handling on stripe hook

--- app/routes/hooks.stripe.tsx
+++ app/routes/hooks.stripe.tsx
@@ -1,18 +1,30 @@
 import type { ActionFunction } from "@remix-run/cloudflare";
 import { json } from "@remix-run/cloudflare";
 import Stripe from "stripe";
+import { z } from "zod";
 
 import {
   getDbFromContext,
   registerPurchase,
 } from "~/services/db.service.server";
 
+const envSchema = z.object({
+  STRIPE_PK: z.string(),
+  STRIPE_WEBHOOK_SECRET: z.string(),
+});
+
 export const action: ActionFunction = async ({ context, request }) => {
+  const parsedEnv = envSchema.safeParse(context);
+  if (!parsedEnv.success) {
+    return json({ error: parsedEnv.error }, { status: 500 });
+  }
+  const env = parsedEnv.data;
+
   const payload = await request.text();
   const sig = request.headers.get("stripe-signature") as string;
 
   let event: Stripe.Event;
-  const stripe = new Stripe(context.STRIPE_PK as string, {
+  const stripe = new Stripe(env.STRIPE_PK, {
     apiVersion: "2022-11-15",
     httpClient: Stripe.createFetchHttpClient(), // ensure we use a Fetch client, and not Node's `http`
   });
@@ -21,27 +33,30 @@ export const action: ActionFunction = async ({ context, request }) => {
     event = await stripe.webhooks.constructEventAsync(
       payload,
       sig,
-      context.STRIPE_WEBHOOK_SECRET as string
+      env.STRIPE_WEBHOOK_SECRET
     );
   } catch (err) {
     console.error(err);
-    return new Response("Webhook Error: " + err, {
-      status: 400,
-    });
+    return json({ error: err }, { status: 400 });
   }
 
   if (event.type === "checkout.session.completed") {
     const session = event.data.object as Stripe.Checkout.Session;
 
     if (!session.customer_details?.name || !session.customer_details?.email) {
-      throw new Error("No name or email found");
+      return json({ error: "Missing customer details" }, { status: 400 });
     }
 
-    const db = getDbFromContext(context);
-    await registerPurchase(db, session.id, {
-      name: session.customer_details.name,
-      email: session.customer_details.email,
-    });
+    try {
+      const db = getDbFromContext(context);
+      await registerPurchase(db, session.id, {
+        name: session.customer_details.name,
+        email: session.customer_details.email,
+      });
+    } catch (err) {
+      console.error(err);
+      return json({ error: err }, { status: 500 });
+    }
   }
 
   return json({ success: true });
