b5988104304a58b9e7dc376748033ade5dc3b6e7
adrienbaron <adrien.baron@tractable.ai>
2023-05-03 00:38:57 +0100

    derive show id from date

--- app/models/shows.ts
+++ app/models/shows.ts
@@ -6,17 +6,17 @@ interface Show {
 
 export const shows: Show[] = [
   {
-    id: "SHOW_ID_1",
+    id: "2023-06-24_12-00-00",
     title: "Enfants I AM 2",
     datetime: new Date("2023-06-24T12:00:00.000Z"),
   },
   {
-    id: "SHOW_ID_2",
+    id: "2023-06-24_15-00-00",
     title: "Enfants I AM 2",
     datetime: new Date("2023-06-24T15:00:00.000Z"),
   },
   {
-    id: "SHOW_ID_3",
+    id: "2023-06-24_19-00-00",
     title: "Adultes I AM 2",
     datetime: new Date("2023-06-24T19:00:00.000Z"),
   },
