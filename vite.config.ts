b1cae21321d71b8584950acafa67f9c1ddaea88d
adrienbaron <adrien@venasecurity.com>
2025-04-18 14:06:20 +0100

    feat: use pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+  plugins: [cloudflareDevProxy(), reactRouter(), tsconfigPaths()],
 });
